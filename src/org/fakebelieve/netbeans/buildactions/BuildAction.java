44ebf70a6453de13166176f628f1a94cad508e38
Joseph Mocker <mock@fakebelieve.org>
2012-06-18 22:25:15 -0700

    initial revisions

---
+++ src/org/fakebelieve/netbeans/buildactions/BuildAction.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fakebelieve.netbeans.buildactions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.tools.ant.module.api.support.ActionUtils;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.ui.OpenProjects;
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionID;
+import org.openide.execution.ExecutorTask;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.NbPreferences;
+import org.openide.util.Task;
+import org.openide.util.TaskListener;
+
+@ActionID(category = "Build",
+id = "org.fakebelieve.netbeans.buildactions.BuildAction")
+@ActionRegistration(iconBase = "org/fakebelieve/netbeans/buildactions/neko.png",
+displayName = "#CTL_BuildAction")
+@ActionReferences({
+    @ActionReference(path = "Toolbars/Build", position = 250)
+})
+@Messages("CTL_BuildAction=Build Action")
+public final class BuildAction implements ActionListener {
+
+    private final DataObject dataContext;
+
+    public BuildAction(DataObject dataContext) {
+        this.dataContext = dataContext;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String path = dataContext.getPrimaryFile().getPath();
+        Project owner = FileOwnerQuery.getOwner(FileUtil.toFileObject(new File(path)));
+        System.err.println("PRIMARY File = " + path);
+        System.err.println("OWNER PROJECT = " + ((owner != null) ? owner.getProjectDirectory() : ""));
+        String actions = NbPreferences.forModule(BuildActionsPanel.class).get("actions", "compile-test");
+        String build = NbPreferences.forModule(BuildActionsPanel.class).get("build", null);
+
+        Project project = (owner != null) ? owner : OpenProjects.getDefault().getMainProject();
+        if (project != null) {
+            try {
+                FileObject mainDirectory = project.getProjectDirectory();
+                System.err.println("PROJECT DIRECTORY = " + mainDirectory.getPath());
+
+                FileObject projectBuildFile = FileUtil.toFileObject(new File(FileUtil.toFile(mainDirectory), "build.xml"));
+
+                if (build != null && build.length() > 0) {
+                    String target = ProjectUtils.getInformation(project).getName();
+                    System.err.println("PROJECT NAME = " + target);
+
+                    final File buildFile = File.createTempFile("build-", ".xml");
+                    buildFile.deleteOnExit();
+                    System.err.println("BUILD FILE = " + buildFile);
+
+                    BufferedWriter out = new BufferedWriter(new FileWriter(buildFile));
+                    out.write(build);
+                    out.close();
+                    Properties properties = new Properties();
+                    properties.put("project-build-file", projectBuildFile.getPath());
+                    ExecutorTask runTarget = ActionUtils.runTarget(FileUtil.toFileObject(buildFile), new String[]{target}, properties);
+                    runTarget.addTaskListener(new TaskListener() {
+
+                        @Override
+                        public void taskFinished(Task task) {
+                            buildFile.delete();
+                        }
+                    });
+                } else {
+                    ActionUtils.runTarget(projectBuildFile, actions.split(","), null);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (IllegalArgumentException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/org/fakebelieve/netbeans/buildactions/BuildActionsOptionsPanelController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.fakebelieve.netbeans.buildactions;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.SubRegistration(location = "Advanced",
+displayName = "#AdvancedOption_DisplayName_BuildActions",
+keywords = "#AdvancedOption_Keywords_BuildActions",
+keywordsCategory = "Advanced/BuildActions")
+@org.openide.util.NbBundle.Messages({"AdvancedOption_DisplayName_BuildActions=Build Actions", "AdvancedOption_Keywords_BuildActions=build actions"})
+public final class BuildActionsOptionsPanelController extends OptionsPanelController {
+
+    private BuildActionsPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    public void applyChanges() {
+        getPanel().store();
+        changed = false;
+    }
+
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    public boolean isChanged() {
+        return changed;
+    }
+
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private BuildActionsPanel getPanel() {
+        if (panel == null) {
+            panel = new BuildActionsPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+}
